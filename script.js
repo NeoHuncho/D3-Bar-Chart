0b8352ace55ca314fb13c43f98ac484a2785414b
NeoHuncho <william.guinaudie@gmail.com>
2020-10-01 11:58:49 +0200

    added more comments

--- script.js
+++ script.js
@@ -2,28 +2,32 @@ let width = 800,
   height = 400,
   barWidth = width / 275;
 
-  //this is the little box than comes up when you put your mouse
+//this is the little box than comes up when you put your mouse
 let tooltip = d3
   .select('.visHolder')
   .append('div')
   .attr('id', 'tooltip')
   .style('opacity', 0);
-//this is the white bar that covers the blue line your are on
+
+  //this is the white bar that covers the blue line your are on
 let overlay = d3
   .select('.visHolder')
   .append('div')
   .attr('class', 'overlay')
   .style('opacity', 0);
-//this is the main svg container
+
+  //this is the main svg container
 let svgContainer = d3
   .select('.visHolder')
   .append('svg')
   .attr('width', width + 100)
   .attr('height', height + 60);
-//here json will fetch data which it will pass into the function
+
+  //here json will fetch data which it will pass into the function
 d3.json(
   'https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json',
   function (data) {
+    
     //adding the text for 'Gross Domestic Product'
     svgContainer
       .append('text')
@@ -31,16 +35,19 @@ d3.json(
       .attr('x', -200)
       .attr('y', 80)
       .text('Gross Domestic Product');
-    //adding text for more info at the bottom
+    
+      //adding text for more info at the bottom
     svgContainer
       .append('text')
       .attr('x', width / 2 + 120)
       .attr('y', height + 50)
       .text('More Information: http://www.bea.gov/national/pdf/nipaguid.pdf')
       .attr('class', 'info');
-//here item is every is each array with date and gdp
+    
+      //here item is every is each array with date and gdp
     let years = data.data.map(function (item) {
       let quarter;
+     
       //temp is the month(there are only 4 months so 4 quaters)
       let temp = item[0].substring(5, 7);
 
@@ -53,57 +60,71 @@ d3.json(
       } else if (temp === '10') {
         quarter = 'Q4';
       }
+      
       // so years returns the date and the quarter
       return item[0].substring(0, 4) + ' ' + quarter;
     });
+   
     //get eah date in the formal format
     let yearsDate = data.data.map(function (item) {
       return new Date(item[0]);
     });
+    
     // xMax is the last date 
     let xMax = new Date(d3.max(yearsDate));
+    
     //not sure about this line adding-removing it doesnt do much
     xMax.setMonth(xMax.getMonth() + 3);
-   // scaleTIme() alligns the different dates with a scale of time
-   // so domain is the min year from yearsDate (above) and xMax is the maxDate(above)
+    
+    // scaleTIme() alligns the different dates with a scale of time
+    // so domain is the min year from yearsDate (above) and xMax is the maxDate(above)
     let xScale = d3
       .scaleTime()
       .domain([d3.min(yearsDate), xMax])
       .range([0, width]);
-//for the bottom xAxis scaled to x
+    
+      //for the bottom xAxis scaled to x
     let xAxis = d3.axisBottom().scale(xScale);
-// appending the xAxis to the svgcontainer
-//the transform property places it in the bottom center (60 is too push a bit too right, 400 is to be a the bottom)
-    svgContainer 
+    
+    // appending the xAxis to the svgcontainer
+    //the transform property places it in the bottom center (60 is too push a bit too right, 400 is to be a the bottom)
+    svgContainer
       .append('g')
       .call(xAxis)
       .attr('id', 'x-axis')
       .attr('transform', 'translate(60, 400)');
-// gets the gdp of each array
+    
+      // gets the gdp of each array
     let GDP = data.data.map(function (item) {
       return item[1];
     });
 
     let scaledGDP = [];
-//max gdp
+   
+    //max gdp
     let gdpMax = d3.max(GDP);
-//linearScale 
+    
+    //linearScale is a function which will return a gdp scaled to the height of the range. the reason why its 0 in the domain and not gdpmin is that so it the graph does not start completly flat 
     let linearScale = d3.scaleLinear().domain([0, gdpMax]).range([0, height]);
-
+    
+    //we then map the GDP into linearScale which will return the heigest gdp to height 400 and then scale each other gdp to a smaller height
     scaledGDP = GDP.map(function (item) {
       return linearScale(item);
     });
-console.log(scaledGDP)
+    
+    //this is to scale the gdp on the yAxis
     let yAxisScale = d3.scaleLinear().domain([0, gdpMax]).range([height, 0]);
-
+// scaling the d3 yaxis to the scale we just made above
     let yAxis = d3.axisLeft(yAxisScale);
 
+    //appending the y axis to the svg (the translate 60 pushes it to the right)
     svgContainer
       .append('g')
       .call(yAxis)
       .attr('id', 'y-axis')
       .attr('transform', 'translate(60, 0)');
 
+     //this is to add all the bars(rect) to the svg
     d3.select('svg')
       .selectAll('rect')
       .data(scaledGDP)
@@ -142,10 +163,10 @@ console.log(scaledGDP)
         tooltip
           .html(
             years[i] +
-              '<br>' +
-              '$' +
-              GDP[i].toFixed(1).replace(/(\d)(?=(\d{3})+\.)/g, '$1,') +
-              ' Billion'
+            '<br>' +
+            '$' +
+            GDP[i].toFixed(1).replace(/(\d)(?=(\d{3})+\.)/g, '$1,') +
+            ' Billion'
           )
           .attr('data-date', data.data[i][0])
           .style('left', i * barWidth + 30 + 'px')
